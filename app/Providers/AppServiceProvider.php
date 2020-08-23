d12d6a05bc83db47b660c6aa937da2de4716eeb6
佐藤周平 <s.s.odn.0617@gmail.com>
2020-08-23 16:21:21 +0900

    fix boot-method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
         //文字列の最大文字数をお191文字に限定する設定
         \Schema::defaultStringLength(191);
         //アプリ内のリンクをhttpsで生成する設定
-        \URL::forceSchema('https');
+        \URL::forceScheme('https');
     }
 
     /**
