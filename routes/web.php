e1886a37b9b0fb684a27778d6070d383f86db373
佐藤周平 <s.s.odn.0617@gmail.com>
2020-08-29 22:26:11 +0900

    create Router

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'RecipesController@index');
+
+Route::resource('recipes', 'RecipesController');
+
+// //レシピの詳細表示
+// Route::get('recipes/{id}', 'RecipesController@show');
+// //レシピの作成の処理
+// Route::post('recipes', 'RecipesController@store');
+// //レシピの更新の処理
+// Route::put('recipes/{id}', 'RecipesController@update');
+// //レシピの削除の処理
+// Route::delete('recipes/{id}', 'RecipesController@destroy');
+// //レシピの一覧を表示
+// Route::get('recipes/{id}', 'RecipesController@show')->name('recipes.index');
+// //レシピ追加のフォームページの表示(storeの補助)
+// Route::get('recipes/create', 'RecipesController@create')->name('recipes.create');
+// //レシピ更新のフォームページの表示(updateの補助)
+// Route::get('recipes/{id}/edit', 'MessagesController@edit')->name('recipes.edit');
