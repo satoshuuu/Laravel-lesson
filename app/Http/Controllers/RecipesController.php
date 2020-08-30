c612bd9ddc430883640ceed3a9b56b744831280b
佐藤周平 <s.s.odn.0617@gmail.com>
2020-08-30 20:06:44 +0900

    add index-controller

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Recipe;
+
 class RecipesController extends Controller
 {
     /**
@@ -14,6 +16,11 @@ class RecipesController extends Controller
     public function index()
     {
         //
+        $recipes = Recipe::all();
+
+        return view('recipes.index', [
+            'recipes' => $recipes,
+        ]);
     }
 
     /**
