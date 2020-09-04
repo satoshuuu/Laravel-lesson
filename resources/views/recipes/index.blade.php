6b3916f50ed7fe8d67bdbf2fb044e0b496151850
佐藤周平 <s.s.odn.0617@gmail.com>
2020-09-04 20:47:35 +0900

    add link-to-show

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -6,15 +6,12 @@
 
   @if (count($recipes) > 0)
     @foreach ($recipes as $recipe)
-    <!-- このリンクだとうまくいかない -->
       <h2>{!! link_to_route('recipes.show', $recipe->recipe_title, ['id' => $recipe->id]) !!}</h2>
       <img src={{ $recipe->img }} alt="">
       <p>{{ $recipe->content }}</p>
     @endforeach
   @endif
 
-  <a href="/recipes/create">新規レシピの投稿</a>
-  <!-- 以下のようにするとリンクがhttps://localhost/recipes/createとなり、うまくつながらない -->
-  <!-- {!! link_to_route('recipes.create', '新規レシピの投稿', [], []) !!} -->
+  {!! link_to_route('recipes.create', '新規レシピの投稿', [], []) !!}
 
 @endsection
